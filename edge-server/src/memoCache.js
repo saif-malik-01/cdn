e893bc555408e7af5c22df2e8678c627462b98e0
Saif Malik <official.saifmalik@gmail.com>
2025-10-01 23:15:50 +0530

    added origin server

--- edge-server/src/memoCache.js
+++ edge-server/src/memoCache.js
@@ -1,20 +1,39 @@
-const cache = new Map();
+//value = { size, expires, etag, path, source, headers }
 
-export function setCache(key, value, ttl = 5000) {
-  cache.set(key, {
-    value,
-    expiry: Date.now() + ttl
-  });
-}
-
-export function getCache(key) {
-  const entry = cache.get(key);
-  if (!entry) return null;
-
-  if (Date.now() > entry.expiry) {
-    cache.delete(key);
-    return null;
+class LRUCache {
+  constructor(limit = 100) {
+    this.limit = limit;
+    this.cache = new Map();
   }
 
-  return entry.value;
+  get(key) {
+    if (!this.cache.has(key)) return null;
+    const value = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
+
+  set(key, value) {
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    } else if (this.cache.size >= this.limit) {
+      const oldestKey = this.cache.keys().next().value;
+      this.cache.delete(oldestKey);
+    }
+
+    this.cache.set(key, value);
+  }
+
+  delete(key) {
+    this.cache.delete(key);
+  }
+
+  has(key) {
+    return this.cache.has(key);
+  }
+
+  size() {
+    return this.cache.size;
+  }
 }
---
+++ origin-server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
